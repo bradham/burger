7c6acd684e6543dbe09108c48c3be9dcd55022ca
bradham <bradham@users.noreply.github.com>
2019-10-27 16:41:36 -0500

    file setup - config, db, and models - need logic for orm.js and burger.js

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Plain');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+
+SELECT *
+FROM `burgers_db`.`burgers`;
---
+++ models/burger.js
@@ -0,0 +1,12 @@
+var orm = require("../config/orm.js");
+
+/* 
+TODO
+create the code that will call the ORM functions using burger specific input for the ORM
+*/
+
+var burger = {
+    //code 
+};
+
+module.exports = burger;
